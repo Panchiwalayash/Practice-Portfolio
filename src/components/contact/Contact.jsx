7e588dfe4afa905d008d4b1635e6e4499dfe034c
Yash Panchiwala <87870272+Panchiwalayash@users.noreply.github.com>
2023-08-19 18:47:23 +0530

    deploying on github

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,38 +1,54 @@
-import './contact.scss'
-import React, {  useState } from 'react'
+import "./contact.scss";
+import React, { useState } from "react";
 
 export default function Contact() {
+  const [message, setMessage] = useState(false);
+  const [data, setData] = useState({ email: "", text: "" });
 
-  const [message,setMessage]=useState(false)
-  const [data,setData]=useState({email:"",text:""})
+  const changeHandler = (e) => {
+    setData({ ...data, [e.target.name]: e.target.value });
+  };
 
-  const changeHandler=(e)=>{
-    setData({...data,[e.target.name]:e.target.value})
-  }
-
-  function clickHandler(e){
-    e.preventDefault()
-    setData({email:"",text:""})
-    setMessage(true)
+  function clickHandler(e) {
+    e.preventDefault();
+    setData({ email: "", text: "" });
+    setMessage(true);
     setTimeout(() => {
-        setMessage(false)
+      setMessage(false);
     }, 2000);
   }
 
   return (
-      <div className="contact" id='contact'>
-        <div className="left">
-          <img src="assets/contact.jpg" alt="" />
-        </div>
-        <div className="right">
-          <div className="contactText">Contact</div>
-          <form className="contactForm" onSubmit={clickHandler}>
-            <input label="Email" placeholder='Email' value={data.email} name='email' onChange={changeHandler} type="Email"  className="formEmail" />
-            <textarea value={data.text} onChange={changeHandler} name="text" placeholder="Enter your text here" rows="4" cols="50"></textarea>
-            <button type='submit'>Send</button>
-            {message && <span>Thanks, we will try to reply as soon as possible </span>}
-          </form>
-        </div>
+    <div className="contact" id="contact">
+      <div className="left">
+        <img src="/ass" alt="" />
       </div>
-  )
+      <div className="right">
+        <div className="contactText">Contact</div>
+        <form className="contactForm" onSubmit={clickHandler}>
+          <input
+            label="Email"
+            placeholder="Email"
+            value={data.email}
+            name="email"
+            onChange={changeHandler}
+            type="Email"
+            className="formEmail"
+          />
+          <textarea
+            value={data.text}
+            onChange={changeHandler}
+            name="text"
+            placeholder="Enter your text here"
+            rows="4"
+            cols="50"
+          ></textarea>
+          <button type="submit">Send</button>
+          {message && (
+            <span>Thanks, we will try to reply as soon as possible </span>
+          )}
+        </form>
+      </div>
+    </div>
+  );
 }
